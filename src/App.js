b36c31c7bf78d419eb0b435acbf90c7dfa263a5d
alina <hodyna.alina@gmail.com>
2021-09-17 13:13:23 +0300

    first implementation style css & props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import ClassCounter from './components/ClassCounter';
+import React from 'react';
+import PostItem from './components/PostItem';
+import './styles/app.css'
 
 function App() {
+
   return (
     <div className="App">
-
-      <ClassCounter />
-
+      <PostItem post={{ id: 1, title: 'JS', body: 'Description' }} />
+      <PostItem post={{ id: 2, title: 'JS', body: 'Description' }} />
+      <PostItem post={{ id: 3, title: 'JS', body: 'Description' }} />
+      <PostItem post={{ id: 4, title: 'JS', body: 'Description' }} />
+      <PostItem post={{ id: 5, title: 'JS', body: 'Description' }} />
     </div>
   );
 }
