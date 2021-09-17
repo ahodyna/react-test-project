33bf65f4e2627ba65e88a649fc70c57287e141f0
alina <hodyna.alina@gmail.com>
2021-09-17 13:22:09 +0300

    rendered  list of posts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostItem from './components/PostItem';
 import './styles/app.css'
 
 function App() {
 
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JS', body: 'Description' },
+    { id: 2, title: 'JS 2', body: 'Description' },
+    { id: 3, title: 'JS 3', body: 'Description' },
+    { id: 4, title: 'JS 4', body: 'Description' },
+    { id: 5, title: 'JS 5', body: 'Description' }
+  ])
+
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: 'JS', body: 'Description' }} />
-      <PostItem post={{ id: 2, title: 'JS', body: 'Description' }} />
-      <PostItem post={{ id: 3, title: 'JS', body: 'Description' }} />
-      <PostItem post={{ id: 4, title: 'JS', body: 'Description' }} />
-      <PostItem post={{ id: 5, title: 'JS', body: 'Description' }} />
+      {posts.map(post =>
+        <PostItem post={post} key={post.id} />
+      )}
     </div>
   );
 }
